5b63dc2d372334999f8e938259c3e22c35e47e7d
snakeJ11 <jeferson.md0212@gmail.com>
2024-03-15 12:48:51 -0400

    commit1

--- app/index.php
+++ app/index.php
@@ -7,7 +7,7 @@
     <h1 >Practica  Docker Compose</h1>
 
 <?php
-        echo "<h2>Jeferson Mercedes</h2>";
+        echo "<h2>Jeferson Mercedes Damaso</h2>";
         echo "<h2>2022-0383</h2>";
     ?>
     
