0e657a181432785ea0fc79ac605627459b93a67f
snakeJ11 <jeferson.md0212@gmail.com>
2024-03-15 12:47:25 -0400

    Realizado

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Docker Compose</title>
+</head>
+<body>
+    <h1 >Practica  Docker Compose</h1>
+
+<?php
+        echo "<h2>Jeferson Mercedes</h2>";
+        echo "<h2>2022-0383</h2>";
+    ?>
+    
+</body>
+</html>
